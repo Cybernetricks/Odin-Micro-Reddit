31f3704611b41b3a57fb8319a7cee6357108b8b5
Cybernetricks <mcgbradley@gmail.com>
2025-11-06 19:38:33 +0000

    Added_email_column_to_User_and_added_post_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 4 }, uniqueness: true
+  validates :email, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_06_162310) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_06_192713) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 end
